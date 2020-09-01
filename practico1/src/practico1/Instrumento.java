fba97f71dea11283fb41ae3fc43e8e26294832ce
Maria Mendez <maria.mendez@est.fi.uncoma.edu.ar>
2020-09-01 11:19:02 -0300

    Primer Commit

---
+++ practico1/src/practico1/Instrumento.java
@@ -0,0 +1,58 @@
+package practico1;
+import java.util.*;
+class Instrumento {
+    public void tocar(){
+        System.out.println("Instrumento.tocar()");
+    }
+public String tipo(){
+    return "Instrumento";
+}
+public void afinar() {};
+}
+
+class Guitarra extends Instrumento {
+    public void tocar(){
+        System.out.println("Guitarra.tocar()");
+    }
+public String tipo(){
+    return "Guitarra";
+}
+public void afinar() {};
+}
+
+class Piano extends Instrumento {
+    public void tocar(){
+        System.out.println("Piano.tocar()");
+    }
+public String tipo() {
+    return "Piano";}
+public void afinar(){}
+}
+
+class Saxofon extends Instrumento {
+    public void tocar(){
+        System.out.println("Saxofon.tocar()");
+    }
+public String tipo() {
+    return "Saxofon";}
+public void afinar(){}
+}
+
+//
+//Un tipo de Guitarra
+class Guzla extends Guitarra {
+public void tocar() {
+System.out.println("Guzla.tocar()");
+}
+public void afinar() {
+System.out.println("Guzla.afinar()");
+}
+}
+
+//Un tipo de Guitarra
+class Ukelele extends Guitarra {
+public void tocar() {
+System.out.println("Ukelele.tocar()");
+}
+public String tipo() { return "Ukelele"; }
+}
