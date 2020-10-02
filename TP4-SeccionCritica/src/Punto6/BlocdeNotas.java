d4ae781aaad046d1e800724552309b8073d79f90
Maria Mendez <maria.mendez@est.fi.uncoma.edu.ar>
2020-10-02 18:23:58 -0300

    para entregar

--- TP4-SeccionCritica/src/Punto6/BlocdeNotas.java
+++ TP4-SeccionCritica/src/Punto6/BlocdeNotas.java
@@ -1,45 +1,46 @@
-  
+
 package Punto6;
 
 import java.util.concurrent.Semaphore;
 
 public class BlocdeNotas {
-	private int contador = 0; //cuenta la cantidad de letras que imprime cada hilo
-	private Semaphore [] turno = new Semaphore[3];
-	
-	public BlocdeNotas(){
-	turno[0]= new Semaphore(1, true);
-	turno[1]= new Semaphore(0, true);
-	turno[2]= new Semaphore(0, true);
+	private int contador = 0; // cuenta la cantidad de letras que imprime cada
+								// hilo
+	private Semaphore[] turno = new Semaphore[3];
+
+	public BlocdeNotas() {
+		turno[0] = new Semaphore(1, true);
+		turno[1] = new Semaphore(0, true);
+		turno[2] = new Semaphore(0, true);
 	}
 
-	private void asignarTurno (int i){
-		try {
-		turno[i].release();
-		}catch (ArrayIndexOutOfBoundsException ex){
+	private void asignarTurno(int i) {
+		if (i < turno.length) {
+			turno[i].release();
+		} else {
 			turno[0].release();
-	}}
-	
-	private void esperarTurno(int i){
+		}
+	}
+
+	private void esperarTurno(int i) {
 		try {
 			turno[i].acquire();
-			}catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	public void escribir(char l, int i){
+
+	public void escribir(char l, int i) {
 		esperarTurno(i);
-		if (contador == i){
+		if (contador == i) {
 			System.out.println(l);
-			contador=0;
-			asignarTurno(i+1);
-		}else {
-				System.out.println(l);
-				turno[i].release();
-				contador=contador +1;
-		}}
-	}	
-
-
+			contador = 0;
+			asignarTurno(i + 1);
+		} else {
+			System.out.println(l);
+			turno[i].release();
+			contador = contador + 1;
+		}
+	}
+}
--- TP4-SeccionCritica/src/Punto8/Testigo.java
+++ TP4-SeccionCritica/src/Punto8/Testigo.java
@@ -4,24 +4,24 @@ import java.util.concurrent.Semaphore;
 
 public class Testigo {
 
-	private Semaphore [] testigo = new Semaphore[4];
-	
-	public Testigo(){
-		testigo[0]= new Semaphore(1, true);
-		testigo[1]= new Semaphore(0, true);
-		testigo[2]= new Semaphore(0, true);
-		testigo[3]= new Semaphore(0, true);
-		}
-	
-	public void pasarTestigo (int i){
-		try {
-		testigo[i].release();
-		}catch (ArrayIndexOutOfBoundsException ex){
+	private Semaphore[] testigo = new Semaphore[4];
+
+	public Testigo() {
+		testigo[0] = new Semaphore(1, true);
+		testigo[1] = new Semaphore(0, true);
+		testigo[2] = new Semaphore(0, true);
+		testigo[3] = new Semaphore(0, true);
+	}
+
+	public void pasarTestigo(int i) {
+		if (i < testigo.length) {
+			testigo[i].release();
+		} else {
 			System.out.println("Termino la carrera");
 		}
 	}
-	
-	public void esperarTestigo(int i){
+
+	public void esperarTestigo(int i) {
 		try {
 			testigo[i].acquire();
 		} catch (InterruptedException e) {
